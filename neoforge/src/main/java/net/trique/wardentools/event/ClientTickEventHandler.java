29a24f912dc89c0445bd83bed21c5c6c973e6873
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-08-28 04:59:26 +0300

    Added missing ClientTickEventHandler

---
+++ neoforge/src/main/java/net/trique/wardentools/event/ClientTickEventHandler.java
@@ -0,0 +1,16 @@
+package net.trique.wardentools.event;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.trique.wardentools.Constants;
+import net.trique.wardentools.util.echolocate.EchoLocateClientHelper;
+
+@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT)
+public class ClientTickEventHandler {
+    @SubscribeEvent
+    private static void clientGlowTick(ClientTickEvent.Pre event) {
+        EchoLocateClientHelper.tickClientGlowingEntities();
+    }
+}
