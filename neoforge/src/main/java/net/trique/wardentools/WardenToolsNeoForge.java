7d99cbb8b0f252bf3a9bea1ca63d1cb833b42307
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-08-31 03:02:00 +0300

    finalize echolocate

--- neoforge/src/main/java/net/trique/wardentools/WardenToolsNeoForge.java
+++ neoforge/src/main/java/net/trique/wardentools/WardenToolsNeoForge.java
@@ -1,21 +1,12 @@
 package net.trique.wardentools;
 
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import net.trique.wardentools.loot.ModLootModifiers;
 import net.trique.wardentools.networking.packet.AddEntityGlowPacket;
-import net.trique.wardentools.platform.Services;
-import net.trique.wardentools.registry.ItemRegistry;
 import net.trique.wardentools.util.echolocate.EchoLocateClientHelper;
 
 @Mod(Constants.MOD_ID)
@@ -31,12 +22,13 @@ public class WardenToolsNeoForge {
         Constants.LOGGER.info("Hello NeoForge world!");
         WardenToolsCommon.init();
         eventBus.addListener(this::setupPackets);
+        ModLootModifiers.register(eventBus);
     }
 
     private void setupPackets(final RegisterPayloadHandlersEvent event) {
         PayloadRegistrar registrar = event.registrar(Constants.MOD_ID).versioned("1").optional();
         registrar.playToClient(AddEntityGlowPacket.TYPE, AddEntityGlowPacket.CODEC, (message, context) -> {
-            context.enqueueWork(() -> EchoLocateClientHelper.addEntity(message.Id(), 300));
+            context.enqueueWork(() -> EchoLocateClientHelper.addEntity(message.id(), 100));
         });
     }
 }
