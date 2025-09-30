ec43918b6d2110664266c8d6dce1aa69c6630a5f
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-10-01 01:57:55 +0300

    rename method in mixin + remove tutorial one

--- common/src/main/java/net/trique/wardentools/mixin/MixinMinecraft.java
+++ common/src/main/java/net/trique/wardentools/mixin/MixinMinecraft.java
@@ -4,28 +4,17 @@ import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
-import net.trique.wardentools.Constants;
 import net.minecraft.client.Minecraft;
 import net.trique.wardentools.registry.EffectRegistry;
 import net.trique.wardentools.util.ClientFunctions;
 import net.trique.wardentools.util.warden_curse.WardenCurseClientHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
-    
-    @Inject(at = @At("TAIL"), method = "<init>")
-    private void init(CallbackInfo info) {
-        
-        Constants.LOGGER.info("This line is printed by an example mod common mixin!");
-        Constants.LOGGER.info("MC Version: {}", Minecraft.getInstance().getVersionType());
-    }
 
     @WrapMethod(method = "shouldEntityAppearGlowing")
-    private boolean renderEchoLocatedEntity(Entity entity, Operation<Boolean> original) {
+    private boolean renderLocatedEntity(Entity entity, Operation<Boolean> original) {
         LocalPlayer player = ClientFunctions.getLocalPlayer();
         return original.call(entity) || (player.hasEffect(EffectRegistry.WARDEN_CURSE) && !entity.is(player) &&
                 WardenCurseClientHelper.getEntitiesToRenderGlowing().contains(entity.getId()));
