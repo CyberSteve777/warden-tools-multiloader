c29c9e83f3d3e5246934118b24ff77a31967886d
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-10-01 23:42:17 +0300

    remove PlayerEntityMixin and move its content to LivingEntityMixin

--- common/src/main/java/net/trique/wardentools/mixin/LivingEntityMixin.java
+++ common/src/main/java/net/trique/wardentools/mixin/LivingEntityMixin.java
@@ -2,7 +2,11 @@ package net.trique.wardentools.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.mojang.serialization.Dynamic;
 import net.minecraft.core.Holder;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffect;
@@ -11,20 +15,34 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.gameevent.DynamicGameEventListener;
+import net.minecraft.world.level.gameevent.vibrations.VibrationSystem;
+import net.trique.wardentools.Constants;
 import net.trique.wardentools.registry.EffectRegistry;
+import net.trique.wardentools.util.warden_curse.WardenCurseUser;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
+import java.util.function.BiConsumer;
 
 
 @Debug(export = true)
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+    @Unique
+    private WardenCurseUser wardentools$wardenCurseUser;
+
+    @Unique
+    private DynamicGameEventListener<VibrationSystem.Listener> wardentools$dynamicGameEventListener;
 
 
     public LivingEntityMixin(EntityType<?> entityType, Level level) {
@@ -33,6 +51,9 @@ public abstract class LivingEntityMixin extends Entity {
 
     @Shadow
     public abstract boolean hasEffect(Holder<MobEffect> effect);
+
+    @Shadow
+    public abstract boolean removeEffect(Holder<MobEffect> effect);
 
     @WrapMethod(method = "canBeAffected")
     private boolean preventDarknessIfSculkAdapted(MobEffectInstance effectInstance, Operation<Boolean> original) {
@@ -44,11 +65,10 @@ public abstract class LivingEntityMixin extends Entity {
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void clearDarknessIfSculkAdapted(CallbackInfo ci) {
-        LivingEntity self = (LivingEntity) (Object) this;
-        if (!self.level().isClientSide()) {
-            if (self.hasEffect(EffectRegistry.SCULK_ADAPTION)) {
-                if (self.hasEffect(MobEffects.DARKNESS)) {
-                    self.removeEffect(MobEffects.DARKNESS);
+        if (!level().isClientSide()) {
+            if (hasEffect(EffectRegistry.SCULK_ADAPTION)) {
+                if (hasEffect(MobEffects.DARKNESS)) {
+                    removeEffect(MobEffects.DARKNESS);
                 }
             }
         }
@@ -56,10 +76,76 @@ public abstract class LivingEntityMixin extends Entity {
 
     @WrapMethod(method = "hurt")
     private boolean reduceSonicBoomDamage(DamageSource source, float amount, Operation<Boolean> original) {
-        LivingEntity self = (LivingEntity) (Object) this;
-        if (self.hasEffect(EffectRegistry.SCULK_ADAPTION) && source.is(DamageTypes.SONIC_BOOM)) {
+        if (hasEffect(EffectRegistry.SCULK_ADAPTION) && source.is(DamageTypes.SONIC_BOOM)) {
             return original.call(source, amount * 0.4f);
         }
         return original.call(source, amount);
     }
+    @Override
+    public boolean dampensVibrations() {
+        return hasEffect(EffectRegistry.SCULK_ADAPTION);
+    }
+
+    @Inject(method = "addAdditionalSaveData", at = @At("TAIL"))
+    private void addVibrationDataFromWardenCurse(CompoundTag compound, CallbackInfo ci) {
+        if (hasEffect(EffectRegistry.WARDEN_CURSE) && wardentools$wardenCurseUser != null) {
+            VibrationSystem.Data.CODEC.encodeStart(NbtOps.INSTANCE, wardentools$wardenCurseUser.getVibrationData())
+                    .resultOrPartial(Constants.LOGGER::error)
+                    .ifPresent(tag -> compound.put("WTVibrationData", tag));
+        }
+    }
+
+    @Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
+    private void readVibrationDataForWardenCurse(CompoundTag compound, CallbackInfo ci) {
+        if (compound.contains("WTVibrationData", 10)) {
+            VibrationSystem.Data.CODEC.parse(new Dynamic<>(NbtOps.INSTANCE, compound.getCompound("WTVibrationData")))
+                    .resultOrPartial(Constants.LOGGER::error).ifPresent(data -> {
+                        if (hasEffect(EffectRegistry.WARDEN_CURSE) && wardentools$wardenCurseUser != null) {
+                            wardentools$wardenCurseUser.setVibrationData(data);
+                        }
+                    });
+        }
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void updateWardenCurseStatus(CallbackInfo ci) {
+        LivingEntity self = (LivingEntity) (Object) this;
+        if (this.level() instanceof ServerLevel level) {
+            if (self.hasEffect(EffectRegistry.WARDEN_CURSE)) {
+                MobEffectInstance echoLocateInstance = self.getEffect(EffectRegistry.WARDEN_CURSE);
+                int amplifier = echoLocateInstance.getAmplifier();
+                Holder<Biome> currentBiome = level.getBiome(self.getOnPos());
+                if (wardentools$wardenCurseUser == null || wardentools$wardenCurseUser.getAmplifier() != amplifier) {
+                    wardentools$wardenCurseUser = new WardenCurseUser(self, amplifier);
+                }
+                if (currentBiome.is(Biomes.DEEP_DARK) && wardentools$wardenCurseUser.getExtraBonus() == 0) {
+                    wardentools$wardenCurseUser.setExtraBonus(16);
+                } else if (wardentools$wardenCurseUser.getExtraBonus() != 0) {
+                    wardentools$wardenCurseUser.setExtraBonus(0);
+                }
+                wardentools$dynamicGameEventListener = new DynamicGameEventListener<>
+                        (new VibrationSystem.Listener(wardentools$wardenCurseUser));
+            } else if (wardentools$wardenCurseUser != null) {
+                wardentools$dynamicGameEventListener = null;
+                wardentools$wardenCurseUser = null;
+            }
+        }
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void tickWardenCurseIfPossible(CallbackInfo ci) {
+        if (wardentools$wardenCurseUser != null && !this.level().isClientSide()) {
+//            Constants.LOGGER.info("amplifier: {}", wardentools$echolocateUser.getAmplifier());
+            VibrationSystem.Ticker.tick(this.level(), wardentools$wardenCurseUser.getVibrationData(),
+                    wardentools$wardenCurseUser.getVibrationUser());
+        }
+    }
+
+    @Override
+    public void updateDynamicGameEventListener(BiConsumer<DynamicGameEventListener<?>, ServerLevel> listenerConsumer) {
+        if (wardentools$dynamicGameEventListener != null && this.level() instanceof ServerLevel level) {
+            listenerConsumer.accept(wardentools$dynamicGameEventListener, level);
+        }
+    }
+
 }
