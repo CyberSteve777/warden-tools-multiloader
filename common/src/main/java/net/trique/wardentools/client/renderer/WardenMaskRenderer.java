34aeaf4a1875907c9c7b4550262f874d1d9e4eab
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-10-20 00:09:56 +0300

    revert custom layer, mask won't be trimmable

--- common/src/main/java/net/trique/wardentools/client/renderer/WardenMaskRenderer.java
+++ common/src/main/java/net/trique/wardentools/client/renderer/WardenMaskRenderer.java
@@ -1,6 +1,5 @@
 package net.trique.wardentools.client.renderer;
 
-import net.trique.wardentools.client.renderer.layer.CustomTrimLayer;
 import net.trique.wardentools.item.melee.WardenMaskItem;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
@@ -9,6 +8,5 @@ import net.trique.wardentools.util.ModHelper;
 public class WardenMaskRenderer extends GeoArmorRenderer<WardenMaskItem> {
     public WardenMaskRenderer() {
         super(new DefaultedItemGeoModel<>(ModHelper.getLoc("armor/warden_mask")));
-        addRenderLayer(new CustomTrimLayer<>(this));
     }
 }
