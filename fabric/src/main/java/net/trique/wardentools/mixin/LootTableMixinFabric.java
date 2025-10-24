b87549a937bbd9fa2e8b7c4e4d3438013bf10e49
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-10-24 23:14:44 +0300

    add null check

--- fabric/src/main/java/net/trique/wardentools/mixin/LootTableMixinFabric.java
+++ fabric/src/main/java/net/trique/wardentools/mixin/LootTableMixinFabric.java
@@ -5,7 +5,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.trique.wardentools.Constants;
 import net.trique.wardentools.LootTableDuck;
 import net.trique.wardentools.util.WTGlobalLootTableUtils;
 import org.spongepowered.asm.mixin.*;
@@ -17,11 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Debug(export = true)
 @Mixin(LootTable.class)
 public abstract class LootTableMixinFabric implements LootTableDuck {
-//
-//    @Shadow @Final private Optional<ResourceLocation> randomSequence;
-//
-//    @Shadow protected abstract ObjectArrayList<ItemStack> getRandomItems(LootContext context);
-
     @Unique
     ResourceLocation id;
 
@@ -32,27 +26,12 @@ public abstract class LootTableMixinFabric implements LootTableDuck {
 
     @Override
     public void warden_tools$setId(ResourceLocation resourceLocation) {
-        Constants.LOGGER.info("Setting id for LootTable: {}", (LootTable)(Object) this);
-        Constants.LOGGER.info("Set ResourceLocation: {}", resourceLocation);
-        id = resourceLocation;
+        if (resourceLocation != null) id = resourceLocation;
     }
 
     @Inject(method = "getRandomItems(Lnet/minecraft/world/level/storage/loot/LootContext;)Lit/unimi/dsi/fastutil/objects/ObjectArrayList;",
-    at = @At("RETURN"))
+            at = @At("RETURN"))
     private void modifyLoot(LootContext context, CallbackInfoReturnable<ObjectArrayList<ItemStack>> cir) {
-        //this is where neoforge modifies loot
-        Constants.LOGGER.info("ResourceLocation: {}", id);
-        WTGlobalLootTableUtils.modifyLoot((LootTable) (Object)this,context,cir.getReturnValue());
+        WTGlobalLootTableUtils.modifyLoot((LootTable) (Object) this, context, cir.getReturnValue());
     }
-
-//    //todo is there a better way to do this?
-//
-//    /**
-//     * @author Tfar
-//     * @reason patch for fabric GLMs
-//     */
-//    @Overwrite
-//    public void getRandomItems(LootParams params, long seed, Consumer<ItemStack> output) {
-//        this.getRandomItems((new LootContext.Builder(params)).withOptionalRandomSeed(seed).create(this.randomSequence)).forEach(output);
-//    }
 }
