43bc6d6f8e002eba57779c57854ebe892ff64d6d
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-10-26 23:36:54 +0300

    finalize Enchantment Helper

--- common/src/main/java/net/trique/wardentools/util/WTEnchantmentHelper.java
+++ common/src/main/java/net/trique/wardentools/util/WTEnchantmentHelper.java
@@ -1,16 +1,18 @@
 package net.trique.wardentools.util;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.trique.wardentools.registry.EnchantmentEffectComponentRegistry;
 import org.apache.commons.lang3.mutable.MutableFloat;
 
 public class WTEnchantmentHelper extends EnchantmentHelper {
-    public float getRangeBonus(ServerLevel serverLevel, ItemStack tool, Entity entity, float projectileSpread) {
-        MutableFloat mutablefloat = new MutableFloat(projectileSpread);
-//        runIterationOnItem(tool, ((enchantmentHolder, level) -> enchantmentHolder.value().modifyDamage()));
+    public static float getRangeBonus(ServerLevel serverLevel, ItemStack tool) {
+        MutableFloat mutablefloat = new MutableFloat();
+        runIterationOnItem(tool, ((enchantmentHolder, level) -> {
+            enchantmentHolder.value().modifyUnfilteredValue(EnchantmentEffectComponentRegistry.INCREASE_RANGE.get(),
+                    serverLevel.getRandom(), level, mutablefloat);
+        }));
         return Math.max(0.0F, mutablefloat.floatValue());
     }
 }
