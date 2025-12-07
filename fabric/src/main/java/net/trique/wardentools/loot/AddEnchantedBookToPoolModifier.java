5b928e8ad8c657bde31f4fa408f3c1ebde306654
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2025-12-08 01:09:45 +0300

    remove unused import

--- fabric/src/main/java/net/trique/wardentools/loot/AddEnchantedBookToPoolModifier.java
+++ fabric/src/main/java/net/trique/wardentools/loot/AddEnchantedBookToPoolModifier.java
@@ -6,7 +6,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.level.storage.loot.LootContext;
